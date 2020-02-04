48e1ca9a82e99f6ae849bac60bc64f091f3c88e3
map <dinhtuananh151020@gmail.com>
2020-02-04 22:42:10 +0700

    add somethings fun

--- anh.js
+++ anh.js
@@ -2,5 +2,5 @@ function anh(argument) {
 	// body...
 	// command line is not work in this site 
 	// backup in mouse.js 
-	
+	int i;
 }
--- cat.js
+++ cat.js
@@ -1,3 +1,6 @@
 function cat(argument) {
 	this.name = name;
+	setTimeout(function() {}, 10);
+	argument.setTimeout;
+
 }
