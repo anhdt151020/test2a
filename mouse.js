2626dfab35e6b2c500acbb5543667baa2c828f27
map <dinhtuananh151020@gmail.com>
2020-02-27 21:12:12 +0700

    testgit

--- mouse.js
+++ mouse.js
@@ -1,3 +1,10 @@
 function function_name(argument) {
 	// body...
 }
+function s(argument) {
+	// body
+	...
+}
+function s(argument){
+	// this is some things fun n 
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+function function_name(argument) {
+	function_name = name;
+	this.name = name;
+	this.oop = new oop;
+}
